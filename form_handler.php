d2be61633a5082e3fd01c4b00fbcfa5ea382fada
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-02-18 14:45:09 +0300

    create form_handler.php

---
+++ form_handler.php
@@ -0,0 +1,17 @@
+<?php
+
+echo $_POST["field-name"];
+
+if (empty($_POST["field-name"])) {
+    echo "<script>alert ('name is empty')</script>";
+}
+
+echo $_POST["field-phone"];
+echo $_POST["field-email"];
+echo $_POST["field-date"];
+echo $_POST["field-gender"];
+echo $_POST["field-pl[]"];
+echo $_POST["field-bio"];
+echo $_POST["check-accept"];
+echo $_POST["button-submit"];
+
