90dd1a2bc5250e9bb5999a546bd1ca63f7c5580b
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-02-18 14:21:25 +0300

    rename index.htmp -> index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="./style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <header>
+        <img src="./images/cat_logo.png" id="logo" alt="Logo">
+        <h1 id="name">Cofee Cat</h1>
+    </header>
+    <div id="content">
+        <section id="form">
+            <h2>Form</h2>
+            <form action="https://www.submit_somewhere.com" method="POST">          
+                <label>Name:
+                    <input name="field-name" placeholder="Your name" type="text">
+                </label>
+                   
+                <label>Phone:
+                    <input name="field-phone" placeholder="Your phone number" type="tel">
+                </label>
+                
+                <label>Email:
+                    <input name="field-email" placeholder="Your email adress" type="email">
+                </label>
+    
+                <label>Date of birth:
+                    <input name="field-date" type="date">
+                </label>
+    
+                <p>Gender:</p>
+                    <label class="radio-gender">
+                        <input name="field-gender" type="radio" value="male">Male
+                    </label>
+                    <label class="radio-gender">
+                    <input name="field-gender" type="radio" value="female">Female
+                    </label>
+    
+                <label>Favorite PL:
+                    <select name="field-pl[]" multiple="multiple">
+                        <option value="pascal">Pascal</option>
+                        <option value="c">C</option>
+                        <option value="cpp">C++</option>
+                        <option value="js">JavaScript</option>
+                        <option value="php">PHP</option>
+                        <option value="python">Python</option>
+                        <option value="java">Java</option>
+                        <option value="haskel">Haskel</option>
+                        <option value="clojure">Clojure</option>
+                        <option value="prolog">Prolog</option>
+                        <option value="scala">Scala</option>
+                    </select>
+                </label>
+    
+                <label>BIO:
+                    <textarea name="field-bio"></textarea>
+                </label>
+    
+                <label id="chkbox-label"><input type="checkbox" name="check-accept"> Accept</label>
+    
+                <label class="submit-button"><input type="submit" name="button-submit"></label>
+            </form>
+        </section>
+    </div>
+    
+</body>
+</html>
