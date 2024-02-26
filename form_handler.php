b4b1e3562b06cecb48a5a241dbdb2e70747ac683
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-02-27 00:12:01 +0300

    add form proceeding and sending to the db

--- form_handler.php
+++ form_handler.php
@@ -28,9 +28,60 @@ function validate_fields () {
     return 1;
 }
 
+function prepare_data () {
+    return;
+}
+
+function connect_to_db () {
+    $user = 'u67423';
+    $pass = '2585011';
+    $db = new PDO('mysql:host=localhost;dbname=test', $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    return $db;
+}
+
+function save_to_db ($db) {
+    $name = $_POST["field-name"];
+    $phone = $_POST["field-phone"];
+    $email = $_POST["field-email"];
+    $bdate = $_POST["field-date"];
+    $gender = $_POST["field-gender"] == "male" ? '1' : '0';
+    $fpl = implode(",", $_POST["field-pl"]);
+    $bio = empty($_POST["field-bio"]) ? '' : $_POST["field-bio"];
+
+    print($name);
+    print("|");
+    print($phone);
+    print("|");
+    print($email);
+    print("|");
+    print($bdate);
+    print("|");
+    print($gender);
+    print("|");
+    print($fpl);
+    print("|");
+    print($bio);
+    print("|");
+
+    $stmt = $db->prepare("INSERT INTO application (name, phone, email, bdate, gender, fpl, bio) VALUES (:name, :phone, :email, :bdate, :gender, :fpl, :bio);");
+    $stmt->bindParam('name', $name);
+    $stmt->bindParam('phone', $phone);
+    $stmt->bindParam('email', $email);
+    $stmt->bindParam('bdate', $bdate);
+    $stmt->bindParam('gender', $gender);
+    $stmt->bindParam('fpl', $fpl);
+    $stmt->bindParam('bio', $bio);
+    $stmt->execute();
+}
+
 if (validate_fields() != 1) {
     $STATUS = validate_fields();
 }
 
-header(sprintf("Location: ?form_save=%d", $STATUS));
+print(implode("\n", $_POST));
+echo "<br>";
+save_to_db(connect_to_db());
+
+
+//header(sprintf("Location: ?form_save=%d", $STATUS));
 
