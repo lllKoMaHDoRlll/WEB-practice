c8840578f4383fb3e69f544d1a221b87266d4822
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-02-26 22:18:07 +0300

    add fields validation function

--- form_handler.php
+++ form_handler.php
@@ -1,19 +1,36 @@
 <?php
 
-echo $_POST["field-name"];
+$STATUS = 1;
 
-if (empty($_POST["field-name"])) {
-    echo "<script>alert ('name is empty')</script>";
+function validate_fields () {
+    
+    if (empty($_POST["field-name"]) || strlen($_POST["field-name"]) > 150 || !preg_match("/^[\p{Cyrillic}a-zA-Z-' ]*$/u", $_POST["field-name"])) {
+        return -1;
+    }
+    if (empty($_POST["field-phone"]) || !preg_match('/[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/i', $_POST["field-phone"])) {
+        return -2;
+    }
+    if (empty($_POST["field-email"]) || !filter_var($_POST["field-email"], FILTER_VALIDATE_EMAIL)) {
+        return -3;
+    }
+    if (empty($_POST["field-date"])) {
+        return -4;
+    }
+    if (empty($_POST["field-gender"])) {
+        return -5;
+    }
+    if (empty($_POST["field-pl"]) || count($_POST["field-pl"]) < 1) {
+        return -6;
+    }
+    if (empty($_POST["check-accept"]) || $_POST["check-accept"] != "accepted") {
+        return -7;
+    }
+    return 1;
 }
 
-echo $_POST["field-phone"];
-echo $_POST["field-email"];
-echo $_POST["field-date"];
-echo $_POST["field-gender"];
-echo $_POST["field-pl[]"];
-echo $_POST["field-bio"];
-echo $_POST["check-accept"];
-echo $_POST["button-submit"];
+if (validate_fields() != 1) {
+    $STATUS = validate_fields();
+}
 
-header("Location: ?form_save=1");
+header(sprintf("Location: ?form_save=%d", $STATUS));
 
