6c0d69fb969a368b7d56975a2b2b58df113d8800
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-02-27 21:25:22 +0300

    implement status code processing

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 
+$STATUS_DESCRIPTION = array(
+    "1" => "Form was sent.",
+    "-1" => "Name field value is incorrect.",
+    "-2" => "Phone field value is incorrect.",
+    "-3" => "Email field value is incorrect.",
+    "-4" => "Date field value is incorrect.",
+    "-5" => "Gender field value is incorrect.",
+    "-6" => "Favorite PL field value is incorrect.",
+    "-7" => "Accept checkbox field value is incorrect.",
+    "-10" => "Error was occured while connecting to the DB.",
+    "-11" => "Error was occured while sending form to the DB."
+);
+
 function on_get() {
+    global $STATUS_DESCRIPTION;
     if(!empty($_GET["form_save"])) {
-        echo "<script>alert ('Form was sent.')</script>";
+        echo sprintf("<script>alert ('%s')</script>", $STATUS_DESCRIPTION[$_GET["form_save"]]);
     }
     include("./index_page.html");
 }
