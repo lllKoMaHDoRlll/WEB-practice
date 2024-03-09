664915b01c89ea8d87a05990624b26ef91e6dbdb
Koma <aleksandr.orlichenya@gmail.com>
2024-03-09 13:06:29 +0300

    implement status alerting with cookies

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php
 
 $STATUS_DESCRIPTION = array(
-    "1" => "Form was sent.",
-    "-1" => "Name field value is incorrect.",
-    "-2" => "Phone field value is incorrect.",
-    "-3" => "Email field value is incorrect.",
-    "-4" => "Date field value is incorrect.",
-    "-5" => "Gender field value is incorrect.",
-    "-6" => "Favorite PL field value is incorrect.",
-    "-7" => "Accept checkbox field value is incorrect.",
-    "-10" => "Error was occured while connecting to the DB.",
-    "-11" => "Error was occured while sending form to the DB."
+    "1" => "Form was successfully sent.",
+    "-1" => "An error was occured during validating fields.",
+    "-2" => "An error was occured during connecting to the database.",
+    "-3" => "An error was occured during serializing fields.",
+    "-4"=> "An error was occured during sending data to the database.",
 );
 
 function on_get()
 {
     global $STATUS_DESCRIPTION;
-    if (!empty($_GET["form_save"])) {
-        echo sprintf("<script>alert ('%s')</script>", $STATUS_DESCRIPTION[$_GET["form_save"]]);
+    if (!empty($_COOKIE["saving_status"])) {
+        echo sprintf("<script>alert ('%s')</script>", $STATUS_DESCRIPTION[$_COOKIE["saving_status"]]);
     }
     include("./index_page.php");
 }
