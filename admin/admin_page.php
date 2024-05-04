d95a739f64bd66b1b82563e070930e6769d38c65
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-04 13:52:45 +0300

    create admin's page php files

---
+++ admin/admin_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../style.css">
+    <title>Admin Page</title>
+</head>
+
+<body>
+    <main class="screen-container">
+        <header>
+            <img src="./../images/cat_logo.png" id="logo" alt="Logo">
+            <h1 id="name">Cofee Cat</h1>
+        </header>
+    </main>
+
+</body>
+
+</html>
