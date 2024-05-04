1b1f77408f3941f35604e5f625a1e0aa518c0b06
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-04 15:26:43 +0300

    implement admin-status module

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -15,7 +15,28 @@
             <h1 id="name">Cofee Cat</h1>
         </header>
         <div class="content">
+            <div class="admin-status tile">
+                <h3>
+                    Administrator
+                    <hr>
+                </h3>
+                <p>
+                    Login: 
+                    <br>
+                    <span>
+                        Some_login
+                    </span>
+                </p>
+                <button>
+                    Exit
+                </button>
+            </div>
+            <div class="submissions-stats tile">
 
+            </div>
+            <div class="user-submissions tile">
+
+            </div>
         </div>
     </main>
 </body>
